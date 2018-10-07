ff66ec2bf9db4115161a1f48285eb1eacf6709fa
SkebloW <skeblow@gmail.com>
2018-10-07 19:39:42 +0200

    [admin] added pdf to mail

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -107,11 +107,15 @@ class OrdersController extends Controller
             switch ($state) {
                 case 0:
                     $mail = new OrderCreated($order, $city, $training, $old_state == 3);
+
                     break;
                 case 1:
-                    $mail = new OrderPaid($order, $city, $training);
-                    break;
+                    $this->generatePdf($order, $training, $city);
 
+                    $mail = new OrderPaid($order, $city, $training);
+
+                    $mail->attach(base_path() . '/files/' . $order->id, ['as' => 'faktura.pdf']);
+                    break;
             }
 
             if (isset($mail)) {
@@ -148,9 +152,215 @@ class OrdersController extends Controller
     }
 
     public function inovice(Request $request, $id) {
-        $mpdf = new Mpdf();
-        $mpdf->WriteHTML('<h1>Hello world!</h1>');
+        $order = Order::findOrFail($id);
+        $training = Training::findOrFail($order->training_id);
+        $city = City::findOrFail($training->city_id);
 
-        return $mpdf->Output();
+        return $this->generatePdf($order, $training, $city)->Output();
+    }
+
+    private function generatePdf(Order $order, Training $training, City $city): Mpdf {
+        $name = $order->parent ?: $order->name;
+        $account = $city->name == 'Jihlava' ? '2001483613/2010' : '2901483600/2010';
+
+        $vystaveni = $order->created_at->format('j.n.Y');
+        $splatnost = clone $order->created_at;
+        $splatnost->add(new \DateInterval('P7D'));
+        $splatnost = $splatnost->format('j.n.Y');
+
+        $mpdf = new Mpdf([
+            'margin_left'   => 21,
+            'margin_right'  => 21,
+            'margin_top'    => 62,
+            'margin_bottom' => 30,
+            'margin_header' => 15,
+            'margin_footer' => 15,
+            'default_font' => 'helvetica'
+        ]);
+
+        $mpdf->SetHTMLHeader('<div class="header-spacer"> </div><div class="header">URBAN SENSE ACADEMY</div>');
+        $mpdf->SetHTMLFooter('<div class="footer">Urban Sense z.s.</div>');
+        $mpdf->WriteHTML('
+            <style>
+              .header {
+                color: #367da2;
+                border-top: 4px solid #367da2;
+                font-weight: bold;
+                font-size: 17px;
+              }
+              .header-spacer {
+                height: 25px;
+              }
+              
+              .footer {
+                text-align: right;
+                border-top: 2px solid #367da2;
+                padding-top: 8px;
+                font-size: 14px;
+              }
+              
+              .faktura-header {
+                font-weight: normal;
+                font-size: 24px;
+              }
+              .blue {
+                color:  #367da2
+              }
+              .do-header {
+                 color: #385d70;
+                 font-style: italic;
+              }
+              .do-text {
+                color: #385d70;
+                font-weight: bold;
+              }
+              .do-spacer {
+                height: 25px;
+              }
+              
+              td {
+                vertical-align: top;
+              }
+              th {
+                text-align: left;
+              }
+              
+              .sumtable {
+                border-collapse: collapse;
+              }
+              
+              .sumtable th {
+                background: #367da2;
+                color: white;
+                padding: 5px;
+              }
+              .sumtable td {
+                 border: 1px dotted #919191;
+                 padding: 5px;
+              }
+              .sumtable .sum td {
+                border-top: 1px solid black;
+                border-bottom: 1px solid black;
+              }
+              
+              .dph {
+                text-align: right;
+              }
+             
+            </style>
+        ');
+        $mpdf->WriteHTML("
+
+
+        
+<table style='width: 100%'>
+<tbody>
+      <tr >
+        <td style='width: 28%'>
+          <h2 class='faktura-header'>FAKTURA</h2>
+          č. <span class='blue'>{$order->id}</span>
+        </td>
+        <td>
+            <div class='do-header'>Odběratel:</div>
+            <div class='do-text'>
+             {$name} <br>
+                &nbsp;<br>
+                &nbsp;
+            </div>
+            <br><br>
+        </td>
+    </tr>
+    <tr>
+      <td valign='top'>
+        <div class='do-header'>Dodavatel:</div>
+        <div class='do-text'>
+            Urban sense <br>
+            U Dvora 4 <br>
+            586 01 Jihlava <br>
+            IČ: 227542211
+        </div>
+        
+      </td>
+      <td>
+        
+          <table style='width: 100%; margin: 0; padding: 0'>
+                <tr>
+                  <td style='width: 40%'>Číslo účtu:</td>
+                  <td>{$account}</td>
+                </tr>
+                <tr>
+                  <td>Název Banky</td>
+                  <td>FIO banka</td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Variabilní symbol</td>
+                  <td>{$order->id}</td>
+                </tr>
+                <tr>
+                  <td>Datum vystavení</td>
+                  <td>{$vystaveni}</td>
+                </tr>
+                <tr>
+                  <td>Datum splatnosti</td>
+                  <td>{$splatnost}</td>
+                </tr>
+                <tr>
+                  <td>Typ platby</td>
+                  <td>Převodem na účet</td>
+                </tr>
+          </table>
+          
+          <br><br>
+          
+          <table class='sumtable'>
+            <tr>
+              <th>Popis</th>
+              <th>Množství</th>
+              <th>Cena/ks</th>
+              <th>Cena</th>
+            </tr>
+            <tr>
+              <td>Půlroční poplatek za kroužek parkouru ve městě {$city->name} ({$training->season})</td>
+              <td>1</td>
+              <td>Kč {$order->price}</td>
+              <td>Kč {$order->price}</td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr class='sum'>
+              <td></td>
+              <td></td>
+              <td>Celkem</td>
+              <td>Kč {$order->price}</td>
+            </tr>
+          </table>
+          
+          <br><br>
+        
+          <table style='width: 100%'><tr><td align='right'>Nejsme pláci DPH.</td></tr></table>
+      </td>
+    </tr>
+</tbody>        
+</table>  
+          
+        ");
+
+        $mpdf->Output(base_path() . '/files/' . $order->id);
+
+        return $mpdf;
     }
 }
